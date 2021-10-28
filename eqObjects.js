0b3d34e12b533743f0a06d6901adcf9bf50094a7
Hamza Warsame <hamza360123@gmail.com>
2021-10-27 22:13:30 -0400

    added eqObjects function, REVIEW NEEDED

---
+++ eqObjects.js
@@ -0,0 +1,46 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+//object eq
+
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if ((Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+//tester
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
