5b5e7621b7883b02568ebafa06694febe2d6d42e
Hamza Warsame <hamza360123@gmail.com>
2021-10-28 14:42:01 -0400

    Add assertObjectEqual function to lotide updates may be needed

---
+++ assertObjectsEqual.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if ((Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+
