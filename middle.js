46274c09f8647d889a56584f629549e9b0c7cc4f
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 19:18:56 -0400

    refactor middle.js on lotide

--- middle.js
+++ middle.js
@@ -1,19 +1,5 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-
-};
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   let mid = [];
@@ -30,6 +16,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1, 2, 4, 5, 6, 8]));
-console.log(middle([1]));
-console.log(middle([1,2,3]));
+module.exports =  middle;
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+
+const middle = require('../middle')
+
+//Test Case
+console.log(middle([1, 2, 4, 5, 6, 8]));
+console.log(middle([1]));
+console.log(middle([1,2,3]));
