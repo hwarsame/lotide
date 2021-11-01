e4027d6d21080fcb6206b8544a93de8438378899
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 18:33:09 -0400

    refactoring lotide

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
-  }
-};
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const head = function(list) {
   return list[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
+
