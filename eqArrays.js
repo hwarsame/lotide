f92037daee82cd05c424380877dfd8407d680273
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 18:57:42 -0400

    refactoring eqArrays on lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed + ${actual} + !== + ${expected}`);
-  }
-};
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual')
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -19,9 +10,7 @@ const eqArrays = function(array1, array2) {
 
 };
 
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
 
 
 
