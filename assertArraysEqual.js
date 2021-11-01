deb19e86b148f9b08cfb73f599fff4fc13302d9b
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 19:14:18 -0400

    Refactor lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,6 @@
 
+const eqArrays = require('./eqArrays');
 
-
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-
-};
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
     console.log(`✅✅✅  Assertion passed ${actual}  ===  ${expected}`);
@@ -18,4 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+module.exports = assertArraysEqual;
--- map.js
+++ map.js
@@ -20,24 +20,24 @@ const eqArrays = function(array1, array2) {
 
 const words = ["ground", "control", "to", "major", "tom"];
 //function below
-const map = function(array, callback){
+const map = function(array, callback) {
   const results = [];
-  for (let item of array){
+  for (let item of array) {
     results.push(callback(item));
   }
   return results;
 
-}
+};
 //let test = map(words, word => word [0]);
 //console.log(test);
 
 
-const testArray = ['Pebble', 'Trebble', 'Hebble', 'Debble', 'Vebble']
+const testArray = ['Pebble', 'Trebble', 'Hebble', 'Debble', 'Vebble'];
 
-let secondLetters = testArray.map(letters => letters[1])
+let secondLetters = testArray.map(letters => letters[1]);
 
-const test2 = ['Hi', 'Bi', 'Ti', 'Yi']
+const test2 = ['Hi', 'Bi', 'Ti', 'Yi'];
 
 let secondLetters2 = test2.map(x => x[1]);
-assertArraysEqual(secondLetters, [ 'e', 'r', 'e', 'e', 'e'])
-assertArraysEqual(secondLetters2, [ 'i', 'i', 'i', 'i']) 
+assertArraysEqual(secondLetters, [ 'e', 'r', 'e', 'e', 'e']);
+assertArraysEqual(secondLetters2, [ 'i', 'i', 'i', 'i']);
