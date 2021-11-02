4a1f96a80c0053fdece3d23c5f130af1d6746fce
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 22:47:14 -0400

    Export functions to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArray = require('./eqArrays');
+const eqObject = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUnit = require('./takeUntil');
+const without = require('./without');
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArray,
+  eqObject,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUnit,
+  without
+};
