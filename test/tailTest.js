8580b34068ad39c92b493b32829864eec9de1475
Hamza Warsame <hamza360123@gmail.com>
2021-11-01 22:25:26 -0400

    refactor tail using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
 
-const assertEqual =  require('../assertEqual');
+/*const assertEqual =  require('../assertEqual');
 const tail = require('../tail');
 
 // TEST CODE
@@ -8,3 +8,19 @@ assertEqual(1, 1);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
+*/
+//Test using Mocha and Chai
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe('#tail', () =>{
+  const words = ['Yo', 'Yo', 'TimHortons'];
+  it("returns the length of the array which is 3", () =>{
+    assert.strictEqual((words.length), 3);
+  });
+  const food = ['Orange', 'Chips'];
+  it("returns a length of 2", () => {
+    assert.strictEqual((food.length), 2);
+  });
+});
